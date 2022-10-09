e20e174780e60c41e4f0b947fe986035d2085b9b
Tyson G Fritz <tysongfritz@gmail.com>
2022-10-09 00:15:10 -0700

    Two ways to access

--- 02/app.js
+++ 02/app.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
       return {
          counter: 0,
          name: "",
+         full_name: "",
       };
    },
    methods: {
@@ -14,6 +15,9 @@ const app = Vue.createApp({
       },
       setName(event) {
          this.name = event.target.value;
+      },
+      setFullName(event, lastName) {
+         this.full_name = event.target.value + " " + lastName;
       },
    },
 });
