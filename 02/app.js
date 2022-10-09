7879caf4c65dcc01b759bbbc3df6396f77d23752
Tyson G Fritz <tysongfritz@gmail.com>
2022-10-09 00:02:01 -0700

    Use a method to increment/decrement count

--- 02/app.js
+++ 02/app.js
@@ -1,9 +1,17 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-    };
-  },
+   data() {
+      return {
+         counter: 0,
+      };
+   },
+   methods: {
+      addCount() {
+         this.counter++;
+      },
+      subCount() {
+         this.counter--;
+      },
+   },
 });
 
-app.mount('#events');
+app.mount("#events");
