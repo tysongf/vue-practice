335ca1eacaddf84be41f50efb66278c16e1629d4
Tyson G Fritz <tysongfritz@gmail.com>
2022-10-10 15:53:03 -0700

    08 Assignment

---
+++ 08-assignment/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+   <active-user
+      v-if="activeUser"
+      :name="activeUser.name"
+      :age="activeUser.age"
+   ></active-user>
+   <user-data @set-active-user="setActiveUser"></user-data>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         activeUser: null,
+      };
+   },
+   methods: {
+      setActiveUser(userName, userAge) {
+         this.activeUser = {
+            name: userName,
+            age: userAge,
+         };
+      },
+   },
+};
+</script>
---
+++ 08-assignment/src/components/UserData.vue
@@ -0,0 +1,32 @@
+<template>
+   <form @submit.prevent>
+      <div>
+         <label for="u_name"></label>
+         <input id="u_name" type="text" v-model="userName" />
+      </div>
+      <div>
+         <label for="u_name"></label>
+         <input id="u_name" type="number" v-model="userAge" />
+      </div>
+      <div>
+         <button @click="submit">Submit</button>
+      </div>
+   </form>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         userName: "",
+         userAge: 0,
+      };
+   },
+   methods: {
+      submit() {
+         this.$emit("set-active-user", this.userName, this.userAge);
+      },
+   },
+   emits: ["set-active-user"],
+};
+</script>
