d48aeb83a579795a316b6591c97acc20bf32c0fb
Tyson G Fritz <tysongfritz@gmail.com>
2022-10-10 12:43:47 -0700

    Pass name, email, phone to FriendContact component using props.

--- 08-props/src/components/FriendContact.vue
+++ 08-props/src/components/FriendContact.vue
@@ -1,37 +1,34 @@
 <template>
-  <li>
-    <h2>{{ friend.name }}</h2>
-    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
-    <ul v-if="detailsAreVisible">
-      <li>
-        <strong>Phone:</strong>
-        {{ friend.phone }}
-      </li>
-      <li>
-        <strong>Email:</strong>
-        {{ friend.email }}
-      </li>
-    </ul>
-  </li>
+   <li>
+      <h2>{{ name }}</h2>
+      <button @click="toggleDetails">
+         {{ detailsAreVisible ? "Hide" : "Show" }} Details
+      </button>
+      <ul v-if="detailsAreVisible">
+         <li>
+            <strong>Phone:</strong>
+            {{ phoneNumber }}
+         </li>
+         <li>
+            <strong>Email:</strong>
+            {{ emailAddress }}
+         </li>
+      </ul>
+   </li>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      detailsAreVisible: false,
-      friend: {
-        id: "manuel",
-        name: "Manuel Lorenz",
-        phone: "0123 45678 90",
-        email: "manuel@localhost.com",
+   props: ["name", "phoneNumber", "emailAddress"],
+   data() {
+      return {
+         detailsAreVisible: false,
+      };
+   },
+   methods: {
+      toggleDetails() {
+         this.detailsAreVisible = !this.detailsAreVisible;
       },
-    };
-  },
-  methods: {
-    toggleDetails() {
-      this.detailsAreVisible = !this.detailsAreVisible;
-    }
-  }
+   },
 };
 </script>
