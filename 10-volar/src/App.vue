4adb3484f6d7771146f18d7679a5186c696da4de
Tyson G Fritz <tysongfritz@gmail.com>
2022-10-11 11:56:52 -0700

    First Volar app and practice project.

---
+++ 10-volar/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+   <the-header title="Remember Me"></the-header>
+   <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from "./components/learning-resources/StoredResources.vue";
+import TheHeader from "./components/layout/TheHeader.vue";
+export default {
+   components: {
+      TheHeader,
+      // "stored-resources": StoredResources, //explicit
+      StoredResources, //shorthand
+   },
+   data() {
+      return {
+         storedResources: [
+            {
+               id: "official-guide",
+               title: "Official Guide",
+               description: "The official Vue.js documentation.",
+               link: "https://vuejs.org",
+            },
+            {
+               id: "google",
+               title: "Google",
+               description: "A heavily censored search engine.",
+               link: "https://google.com",
+            },
+         ],
+      };
+   },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+   box-sizing: border-box;
+}
+
+html {
+   font-family: "Roboto", sans-serif;
+}
+
+body {
+   margin: 0;
+}
+</style>
