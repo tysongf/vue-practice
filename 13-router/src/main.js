960d424fb5d097483a73c9b30177d754cbf9a4a1
Tyson G Fritz <tysongfritz@gmail.com>
2022-10-13 22:27:36 -0700

    implement router-link and router-view and CSS classes for active links.

--- 13-router/src/main.js
+++ 13-router/src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+import TeamsList from "./components/teams/TeamsList.vue";
+import UsersList from "./components/users/UsersList.vue";
+
+const app = createApp(App);
+const router = createRouter({
+   history: createWebHistory(),
+   routes: [
+      { path: "/teams", component: TeamsList },
+      { path: "/users", component: UsersList },
+   ],
+   linkActiveClass: "active",
+   linkExactActiveClass: "exact-active",
+});
+
+app.use(router);
+
+app.mount("#app");
