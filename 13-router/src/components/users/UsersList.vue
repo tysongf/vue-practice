82cb7ee7ae880e645c67a104e53aab1f51a8d550
Tyson G Fritz <tysongfritz@gmail.com>
2022-10-14 20:19:06 -0700

    Programmatic router link

--- 13-router/src/components/users/UsersList.vue
+++ 13-router/src/components/users/UsersList.vue
@@ -1,25 +1,36 @@
 <template>
-  <ul>
-    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
-  </ul>
+   <button @click="confirmInput">Programmatic Link</button>
+   <ul>
+      <user-item
+         v-for="user in users"
+         :key="user.id"
+         :name="user.fullName"
+         :role="user.role"
+      ></user-item>
+   </ul>
 </template>
 
 <script>
-import UserItem from './UserItem.vue';
+import UserItem from "./UserItem.vue";
 
 export default {
-  components: {
-    UserItem,
-  },
-  inject: ['users'],
+   components: {
+      UserItem,
+   },
+   inject: ["users"],
+   methods: {
+      confirmInput() {
+         this.$router.push("/teams");
+      },
+   },
 };
 </script>
 
 <style scoped>
 ul {
-  list-style: none;
-  margin: 2rem auto;
-  max-width: 20rem;
-  padding: 0;
+   list-style: none;
+   margin: 2rem auto;
+   max-width: 20rem;
+   padding: 0;
 }
 </style>
